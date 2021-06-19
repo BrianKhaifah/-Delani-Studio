6d0d88ac6d415a1e106d0dcd44e77b6c4966fc44
BrianKhaifah <briankhaifah@gmail.com>
2021-06-19 17:36:14 +0300

    added a hover function on script.js for portfolio

--- js/script.js
+++ js/script.js
@@ -1,23 +1,18 @@
 $(document).ready(function(){
-    // $("#design").click(function(){
-    //     $("design-p").toggle();
-    // });
-
-    // $("#dev").click(function(){
-    //     $("dev-p").toggle();
-    // });
-
-
-    // $("#product").click(function(){
-    //     $("product-p").toggle();
-    // });
-
-    // $("p").css("text-align","center")
 
     $(".main").click(function(){
         $(this).children("#design-p").slideToggle(1500);
         $(this).children("#design").toggle();
     });
+
+    $(".rap").mouseover(function()
+    {
+        $(this).children(".name").show();
+    })
+    .mouseout(function(){
+        $(this).children(".name").hide();
+    })
+    
 
    
 
