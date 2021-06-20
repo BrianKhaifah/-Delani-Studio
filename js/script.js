c4e6d59b03a256243d796ee6a94fd9511369ea88
BrianKhaifah <briankhaifah@gmail.com>
2021-06-20 10:22:40 +0300

    updatedthe form list on html and function script.js

--- js/script.js
+++ js/script.js
@@ -14,3 +14,34 @@ $(document).ready(function(){
     })
     
 });
+function main(){
+    var name = document.getElementById("mce-NAME").nodeValue;
+    var name = document.getElementById("mce-EMAIL").nodeValue;
+    var name = document.getElementById("mce-MESSAGE").nodeValue;
+    var mailFormat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+    var validateForm = function() {
+        if (name =="" || email == "" || message == ""){
+            alert("Please fill in the form");
+        }
+        else {
+            return true;
+        };
+        if(email !== mailFormat){
+            alert("Input the correct email")
+        }
+        else {
+            return true;
+        };
+    }
+
+    if (validateForm() === true ) {
+        alert (" We have received your message " + name + ". Thank you for reaching out to us")
+
+    }
+    else {
+        return false;
+    };
+
+
+};
