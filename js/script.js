08e0a7e761d0e1e0bc8d9351a6bd885684658447
BrianKhaifah <briankhaifah@gmail.com>
2021-06-20 13:12:12 +0300

    Updated the alert note on script file

--- js/script.js
+++ js/script.js
@@ -12,36 +12,25 @@ $(document).ready(function(){
     .mouseout(function(){
         $(this).children(".name").hide();
     })
+    $(".form").submit(function(){
+        var name = document.getElementById("mce-NAME").value
+        var email = document.getElementById("mce-EMAIL").value
+        var message = document.getElementById("mce-MESSAGE").value
+        if (name == "" || name == null){
+            alert("Name can not be empty")
+        }
+        else if (email == "" || email == null){
+            alert("Email can not be empty")
+        }
+
+        else if (message == "" || message == null){
+            alert("message can not be empty")
+        }
+       else{
+        alert (" We have received your message " + name + ". Thank you for reaching out to us")
+       }
+        
+
+    })
     
 });
-function main(){
-    var name = document.getElementById("mce-NAME").nodeValue;
-    var name = document.getElementById("mce-EMAIL").nodeValue;
-    var name = document.getElementById("mce-MESSAGE").nodeValue;
-    var mailFormat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-
-    var validateForm = function() {
-        if (name =="" || email == "" || message == ""){
-            alert("Please fill in the form");
-        }
-        else {
-            return true;
-        };
-        if(email !== mailFormat){
-            alert("Input the correct email")
-        }
-        else {
-            return true;
-        };
-    }
-
-    if (validateForm() === true ) {
-        alert (" We have received your message " + name + ". Thank you for reaching out to us")
-
-    }
-    else {
-        return false;
-    };
-
-
-};
